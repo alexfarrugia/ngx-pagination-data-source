d51b4dc03cba89c7b7b42cd2a39b14327e0e03a6
alexfarrugia <alexfar@gmail.com>
2024-08-01 16:09:15 +0200

    fixed node version for build

--- projects/ngx-pagination-data-source/src/test.ts
+++ projects/ngx-pagination-data-source/src/test.ts
@@ -4,8 +4,7 @@ import 'zone.js'
 import 'zone.js/testing'
 import {getTestBed} from '@angular/core/testing'
 import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+  BrowserDynamicTestingModule, platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing'
 
 declare const require: {
@@ -18,6 +17,8 @@ declare const require: {
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
+  // @ts-ignore
+
   platformBrowserDynamicTesting()
 )
 // Then we find all the tests.
